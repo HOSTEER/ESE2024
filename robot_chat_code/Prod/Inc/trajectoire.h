b6af057cab581bffb7a5c68703a806263176b178
unknown <teoman42@icloud.com>
2023-12-13 12:07:13 +0100

    follow trajectory

---
+++ robot_chat_code/Prod/Inc/trajectoire.h
@@ -0,0 +1,19 @@
+
+
+#ifndef INC_TRAJECTOIRE_H_
+#define INC_TRAJECTOIRE_H_
+
+#include "main.h"
+
+#define MAX_POS_ERROR (5<<16) //mm Q.16
+
+typedef struct trajectory_t_struct {
+	int32_t x,y;
+	int32_t speed;
+	int32_t angle_init,angle_end;
+
+} trajectory_t;
+
+void follow_trajectory(hOdometry_t hOdometry, int32_t x, int32_t y, int32_t *angle, int32_t *mot_speed);
+
+#endif /* INC_TRAJECTOIRE_H_ */
